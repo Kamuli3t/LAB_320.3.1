391d1758563fbe5f155f352fd394c21e925729f8
kamuli3t <kamuli3t@gmail.com>
2025-03-03 04:59:28 -0800

    initialize React project with Vite, add main components and data structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import learnersData from "./data/learnersData";
+
+function App() {
+  const [learners, setLearners] = useState(learnersData);
+  return <></>;
+}
+
+export default App;
